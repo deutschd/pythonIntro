c41f9146fbb285e838edcd16053544a1d076f19c
deutschd <ismailovshakhzod@gmail.com>
2022-03-03 16:45:37 +0500

    Operations with String / calc / area of a shape

---
+++ codeingame/strings_exercises.py
@@ -0,0 +1,42 @@
+print('find an area of a triangle with 3 sides\n')
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# p = (a+b+c)/2
+# S = (p*(p-a)*(p-b)*(p-c))**0.5
+# print(S)
+
+print('the number between the intervals\n')
+# x = int(input())
+# p_inf = float('inf')
+# if -15 < x <= 12 or 14 < x < 17 or 19 <= x <= p_inf:
+#     print(True)
+# else:
+#     print(False)
+
+print('calculator=> +, -, /, *, mod = %, pow = **, div = //\n')
+# a = float(input())
+# b = float(input())
+# c = input()  # given Math symbol
+# if c == '+':
+#     print(a + b)
+# elif c == '-':
+#     print(a - b)
+# elif c == '/' and b == 0:
+#     print('Деление на 0!')
+# elif c == '/' and b != 0:
+#     print(a / b)
+# elif c == '*':
+#     print(a * b)
+# elif c == 'mod' and b == 0:
+#     print('Деление на 0!')
+# elif c == 'mod' and b != 0:
+#     print(a % b)
+# elif c == 'pow':
+#     print(a ** b)
+# elif c == 'div' and b == 0:
+#     print('Деление на 0!')
+# elif c == 'div' and b != 0:
+#     print(a // b)
+
