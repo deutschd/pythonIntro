d436f7915e43c2ac7c69b159c4a8e45b459dee90
deutschd <ismailovshakhzod@gmail.com>
2022-03-04 23:38:49 +0500

    Operations with String / calc / area of a shape

--- codeingame/strings_exercises.py
+++ codeingame/strings_exercises.py
@@ -1,4 +1,4 @@
-print('find an area of a triangle with 3 sides\n')
+print('find an area of a triangle with 3 given sides\n')
 # a = int(input())
 # b = int(input())
 # c = int(input())
@@ -40,3 +40,53 @@ print('calculator=> +, -, /, *, mod = %, pow = **, div = //\n')
 # elif c == 'div' and b != 0:
 #     print(a // b)
 
+print('find an area of a object = triangle, rectanle, circle\n')
+# pi = 3.14
+# chosen_object = input()
+# if chosen_object == 'треугольник':
+#     a = int(input())
+#     b = int(input())
+#     c = int(input())
+#     p = (a + b + c) / 2
+#     S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+#     print(S)
+# if chosen_object == 'прямоугольник':
+#     a = int(input())
+#     b = int(input())
+#     print(a * b)
+# if chosen_object == 'круг':
+#     r = int(input())
+#     print(pi * (r ** 2))
+
+print('one max -> one min -> any\n')
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a <= b < c or a < b < c or a < b <= c:
+#     print(c)
+#     print(a)
+#     print(b)
+# elif b <= a < c or b < a < c or b < a <= c:
+#     print(c)
+#     print(b)
+#     print(a)
+# elif b <= c < a or b < c < a or b < c <= a:
+#     print(a)
+#     print(b)
+#     print(c)
+# elif c <= b < a or c < b < a or c < b <= a:
+#     print(a)
+#     print(c)
+#     print(b)
+# elif c <= a < b or c < a < b or c < a <= b:
+#     print(b)
+#     print(c)
+#     print(a)
+# elif a <= c < b or a < c < b or a < c <= b:
+#     print(b)
+#     print(a)
+#     print(c)
+# else:
+#     print(a)
+#     print(b)
+#     print(c)
