b56550b492fce93cf0f9a7f2f1873b09620baf7f
deutschd <ismailovshakhzod@gmail.com>
2022-07-06 23:16:05 +0500

    Recall| String Formatting + Templates + Exercise 1

--- recall.py
+++ recall.py
@@ -1,4 +1,7 @@
+import string
+
 print('Hello World')
+
 print('"Hello ", + user_name\n')
 # user_name = input('Enter your name: ')
 # print('Hello ', user_name)
@@ -14,7 +17,80 @@ print("Python Statements\n")
 #       number_of_people_in_city,
 #       'in',
 #       year)
-print("Splitting Strings")
+print("Splitting Strings - based on the character(method)\n")  # methods are applied with dot notation
+# title = 'The Good, The Bad, and the Ugly'
+# print('Source string', title)
+# print(title.split())  # = print(title.split(' '))
+# print(title.split('a'))
+print("Counting Strings - how many times a string is repeated\n")
+# my_string = 'Count, the number    of spaces'
+# print("my_string.count(' '):", my_string.count(' '))
+print("Replacing Strings - .replace(old, new)\n")
+# welcome_message = 'Hello World!'
+# print(welcome_message.replace("Hello", "Goodbye"))
+print("Finding Sub Strings - string.find('A') -> where it starts\n")
+# print('Edward Alun Rawlings'.find('2'))
+# print('Hello this is a new way to start'.find('star'))
+print("Other String Operations\n")
+# text = 'hello World'
+# print("Testing a string")
+# print(text.startswith('H'))
+# print(text.startswith('h'))
+# print(text.endswith('d'))
+# print(text.istitle())
+# print(text.isupper())
+# print(text.islower())
+# print(text.isalpha())
+#
+# print("\nString conversions")
+# print(text.upper())
+# print(text.lower())
+# print(text.title())
+# print(text.swapcase())
+# print('String leading, trailing spaces', "       hel    ale   ".strip())
+
+print("String Formatting\n")
+# format_string = 'Hello {}!'
+# print(format_string.format('Phoebe'))  # Hello Phoebe!
+#
+# name = "Adam"
+# age = 20
+# print("{} is {} years old".format(name, age))  # Adam is 20 y.o.
+#
+# format_string = "Hello {1} {0}, you got {2}%"
+# print(format_string.format('Smith', 'Carol', 75))  # Hello Carol Smith, you got 75%
+#
+# format_string = "{artist} sang {song} in {year}"
+# print(format_string.format(artist='Paloma Faith', song='Guilty', year=2022))
+#
+# print('|{:25}|'.format('left aligned'))  # :>right, :^centered :<ok
+# print('|{:>25}|'.format('right aligned'))
+# print('|{:^25}|'.format('centered'))
+#
+# print('{:,}'.format(12342132131321))
+# print('{:,}'.format(1234567890.0))
+print("String Templates\n")
+# template = string.Template('$artist sang $song in $year')
+# print(template.substitute(artist='Freddie Mercury',
+#                           song='The Great Pretender', year=1987))
+# print(template.substitute(artist='Ed Sheeran', song='Galway Girl', year=2017))
+#
+# d = dict(artist='Billy', song='Eyes Without a Face', year=1984)
+# print(template.substitute(d))
+
+print('Exercise 1\n')
+# print('Denyse,Marie,Smith,21,London,UK'.replace(',', ' '))
+# a = str(input('Write anything and it will be together, Let\'s the first one: '))
+# b = str(input('Second one: '))
+# new_string = a + ' ' + b
+# print(new_string)
+# print(len(new_string))
+# print(new_string.upper())
+# print(new_string.find('Albus'))
+
+
+
+
 
 
 
