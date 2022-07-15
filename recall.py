3646da41a02e390b089faa8714349ba34a03886d
deutschd <ismailovshakhzod@gmail.com>
2022-07-15 23:00:35 +0500

    for loop -> start

--- recall.py
+++ recall.py
@@ -159,6 +159,30 @@ while count < 10:
     count += 1
 print('Done')
 print("For Loop\n")
+# for i in range(0, 10, 2):
+#     print(i, '', end='')
+# print('\nDone')
+# for _ in range(0,10):
+#     print('.', end='')
+print("Break Loop Statement\n")
+print('Only print code if all iterations completed')
+# num = int(input('Enter a number to check for: '))
+# for i in range(0, 6):
+#     if i == num:
+#         break
+#     print(i, '', end='')
+# print('Done')
+print("Continue Loop Statement\n")
+for i in range(0, 10):
+    print(i, '', end='')
+    if i % 2 == 1:
+        continue
+    print('hey its an even number')
+    print('we love even numbers')
+print('Done')
+
+
+
 
 
 
