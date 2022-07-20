cf99cd17baf43e3bbdb1a133e78ef4522be52b6d
deutschd <ismailovshakhzod@gmail.com>
2022-07-20 14:46:59 +0500

    Anonymous Functions (lambda)

--- john_hunt_python3/true_step_one.py
+++ john_hunt_python3/true_step_one.py
@@ -77,9 +77,68 @@ my_function('Paul', 'Fiona', son_number_one='Andrew',
             son_number_two='James', daughter='Joselyn')
 
 print("\nDefine methods that only use of the *args && **kwargs")
+
+
 def named(**kwargs):
     for key in kwargs.keys():
         print('arg:', key, 'has value:', kwargs[key])
 
 
 named(a=1, b=2, c=3)
+
+print("\nAnonymous Functions:")
+number_doubled = lambda i: i * i
+print(number_doubled(10))
+func0 = lambda: print('no args')
+func1 = lambda x: x * x
+func2 = lambda x, y: x * y
+func3 = lambda x, y, z: x + y + z
+
+func0()
+print(func1(4))
+print(func2(3, 4))
+print(func3(2, 3, 4))
+
+print("\nScope and Lifetime of Variables")
+max = 100
+
+
+def print_max():
+    global max
+    max = max + 1
+    print(max)
+
+
+print_max()
+print(max)
+
+print("\nNonlocal Variables:")
+
+
+def outer():
+    title = 'original title'
+
+    def inner():
+        title = 'another title'
+        print('inner:', title)
+
+    inner()
+    print('outer:', title)
+
+
+outer()
+
+
+def outer():
+    title = 'original title'
+
+    def inner():
+        nonlocal title
+        title = 'another title'
+        print('inner:', title)
+
+    inner()
+    print('outer:', title)
+
+
+outer()
